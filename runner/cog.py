fc4fbbd8436b877f941f0201c347900e5f423baf
disal <disal@admin.git>
2024-07-04 13:32:48 +0000

    implement async streaming stdout and async create task

--- runner/cog.py
+++ runner/cog.py
@@ -70,7 +70,6 @@ def run(
         trained_model=trained_model,
         job_id=job_id
     )
-    at = replace_source_with_destination(at, base_dir)
     print(run_script)
     # stdout_file_path = Path(f"{base_dir}/{str(task_id)}/stdout.log").resolve()
     # process = executor.submit(
